56b10e127152c407eb643a754032d76552f0f1a5
Zeth <zeth@zethjack.eu>
2024-03-25 13:47:49 +0100

    wofi-pass doesnt use store location?

--- homeManagerModules/features/hyprland/default.nix
+++ homeManagerModules/features/hyprland/default.nix
@@ -215,7 +215,7 @@ in {
             "$mainMod SHIFT, D, exec, wofi --show drun"
             "$mainMod, D, exec, rofi -show drun -show-icons"
             "$mainMod, P, pin, active"
-            "CTRL ALT, P, exec, wofi-pass"
+            "CTRL ALT, P, exec, wofi-pass ~/.local/share/password-store"
 
             "$mainMod, left, movefocus, l"
             "$mainMod, right, movefocus, r"
