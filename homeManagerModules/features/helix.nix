184173cf11ef00ce0700ed215b89dd2d8b07e615
Zeth <zeth@zethjack.eu>
2024-05-11 05:20:08 +0200

    added helix config

---
+++ homeManagerModules/features/helix.nix
@@ -0,0 +1,43 @@
+{ pkgs,
+... }:
+{
+  programs.helix = {
+    enable = "true";
+    defaultEditor = "true";
+    settings = {
+      
+    theme = "catppuccin_mocha";
+    keys.normal = {
+      esc = [ "collapse_selection" "keep_primary_selection" ];
+    };
+    editor = {
+      line-number = "relative";
+      bufferline = "multiple";
+      cursor-shape = {
+        insert = "bar";
+      };
+      indent-guides = {
+        render = true;
+        character = "┆";
+        skip-levels = 1;
+      };
+
+      soft-wrap = {
+        enable=true;
+        max-wrap=25;
+        };
+    };
+
+    };
+  };
+  
+  home.file.".local/bin/lf-pick".text = ''
+    function lfp(){
+      local TEMP=$(mktemp)
+      lf -selection-path=$TEMP
+      cat $TEMP
+    }
+
+    lfp
+  '';
+}
