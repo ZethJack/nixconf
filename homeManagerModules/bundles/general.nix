9ad9830a85998706722e9b8aae4906e97147c847
Zeth <zeth@zethjack.eu>
2024-05-17 04:53:43 +0200

    Added config for hyprlock

--- homeManagerModules/bundles/general.nix
+++ homeManagerModules/bundles/general.nix
@@ -22,6 +22,7 @@
   myHomeManager.nix-extra.enable = lib.mkDefault true;
   myHomeManager.bottom.enable = lib.mkDefault true;
   myHomeManager.nix-direnv.enable = lib.mkDefault true;
+  myHomeManager.hyprlock.enable = lib.mkDefault true;
 
   programs.home-manager.enable = true;
   programs.git = {
@@ -45,41 +46,6 @@
     tray.enable = false;
   };
 
-  programs.hyprlock = {
-    enable = true;
-    settings = {
-      general = {
-        disable_loading_bar = true;
-        grace = 300;
-        hide_cursor = true;
-        no_fade_in = false;
-      };
-
-      background = [
-        {
-          path = "screenshot";
-          blur_passes = 3;
-          blur_size = 8;
-        }
-      ];
-
-      input-field = [
-        {
-          size = "200, 50";
-          position = "0, -80";
-          monitor = "";
-          dots_center = true;
-          fade_on_empty = false;
-          font_color = "rgb(202, 211, 245)";
-          inner_color = "rgb(91, 96, 120)";
-          outer_color = "rgb(24, 25, 38)";
-          outline_thickness = 5;
-          shadow_passes = 2;
-        }
-      ];
-    };
-
-  };
 
   home.packages = with pkgs; [
     nil
---
+++ homeManagerModules/features/hyprlock.nix
@@ -0,0 +1,52 @@
+{pkgs, ...}:
+{
+  programs.hyprlock = {
+    enable = true;
+    settings = {
+      general = {
+        disable_loading_bar = true;
+        grace = 2;
+        hide_cursor = true;
+        no_fade_in = false;
+      };
+
+      background = [
+        {
+          path = "screenshot";
+          blur_passes = 3;
+          blur_size = 8;
+        }
+      ];
+
+      input-field = [
+        {
+          size = "200, 50";
+          position = "0, -80";
+          monitor = "";
+          dots_center = true;
+          fade_on_empty = false;
+          font_color = "rgb(202, 211, 245)";
+          inner_color = "rgb(91, 96, 120)";
+          outer_color = "rgb(24, 25, 38)";
+          outline_thickness = 5;
+          shadow_passes = 2;
+        }
+      ];
+      labels = [
+        {
+          # clock
+          text = "$TIME";
+          color = "rgb(202, 211, 245)";
+          font_size = 65;
+          position = {
+              x = 0;
+              y = 300;
+          };
+          halign = "center";
+          valign = "center";
+
+        }
+      ];
+    };
+  };
+}
