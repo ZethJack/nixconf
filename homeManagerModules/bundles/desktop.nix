29ae741850f3d7020f8775bc3aca5b15a7385bc5
Zeth <zeth@zethjack.eu>
2024-07-10 19:21:21 +0200

    added linkhandler script

--- homeManagerModules/bundles/desktop.nix
+++ homeManagerModules/bundles/desktop.nix
@@ -24,6 +24,7 @@
     myHomeManager.helix.enable = lib.mkDefault true;
     myHomeManager.sysact.enable = lib.mkDefault true;
     myHomeManager.grimslurp.enable = lib.mkDefault true;
+    myHomeManager.linkhandler.enable = lib.mkDefault true;
 
     myHomeManager.gtk.enable = lib.mkDefault true;
 
---
+++ homeManagerModules/features/linkhandler/default.nix
@@ -0,0 +1,11 @@
+{pkgs, ...}: let
+linkhandler = pkgs.writeShellScriptBin "linkhandler" (builtins.readFile ./linkhandler);
+qndl = pkgs.writeShellScriptBin "qndl" (builtins.readFile ./qndl);
+
+in
+{
+  home.packages = with pkgs; [
+    linkhandler
+    qndl
+  ];
+}
