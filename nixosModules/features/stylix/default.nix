80d7959b328851c390ba0bd3e7be20c96e67f53b
Zeth <zeth@zethjack.eu>
2024-10-27 00:08:22 +0200

    Stylix 2 the electric boogaloo

--- nixosModules/features/stylix/default.nix
+++ nixosModules/features/stylix/default.nix
@@ -1,48 +1,39 @@
 {
   pkgs,
   inputs,
+  lib,
   ...
-}: {
+}: 
+let schemeAttr = file:
+      (builtins.fromJSON (builtins.readFile (pkgs.runCommand "json" { } ''
+      ${lib.getExe pkgs.yj} < "${file}" > $out
+      '').outPath)).palette;
+in
+{
+
   imports = [
     inputs.stylix.nixosModules.stylix
   ];
   stylix = {
+    base16Scheme = schemeAttr "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     # base16Scheme = {
-    #  base00 = "272822"; # ----
-    #  base01 = "f92672"; # ---
-    #  base02 = "a6e22e"; # --
-    #  base03 = "f4bf75"; # -
-    #  base04 = "66d9ef"; # +
-    #  base05 = "d5c4a1"; # ++
-    #  base06 = "ebdbb2"; # +++
-    #  base07 = "fbf1c7"; # ++++
-    #  base08 = "fb4934"; # red
-    #  base09 = "fe8019"; # orange
-    #  base0A = "fabd2f"; # yellow
-    #  base0B = "b8bb26"; # green
-    #  base0C = "8ec07c"; # aqua/cyan
-    #  base0D = "7daea3"; # blue
-    #  base0E = "e089a1"; # purple
-    #  base0F = "f28534"; # brown
+    #   base00 = "1e1e2e"; # base
+    #   base01 = "181825"; # mantle
+    #   base02 = "313244"; # surface0
+    #   base03 = "45475a"; # surface1
+    #   base04 = "585b70"; # surface2
+    #   base05 = "cdd6f4"; # text
+    #   base06 = "f5e0dc"; # rosewater
+    #   base07 = "b4befe"; # lavender
+    #   base08 = "f38ba8"; # red
+    #   base09 = "fab387"; # peach
+    #   base0A = "f9e2af"; # yellow
+    #   base0B = "a6e3a1"; # green
+    #   base0C = "94e2d5"; # teal
+    #   base0D = "89b4fa"; # blue
+    #   base0E = "cba6f7"; # mauve
+    #   base0F = "f2cdcd"; # flamingo
     # };
-    base16Scheme = {
-      base00 = "1e1e2e"; # base
-      base01 = "181825"; # mantle
-      base02 = "313244"; # surface0
-      base03 = "45475a"; # surface1
-      base04 = "585b70"; # surface2
-      base05 = "cdd6f4"; # text
-      base06 = "f5e0dc"; # rosewater
-      base07 = "b4befe"; # lavender
-      base08 = "f38ba8"; # red
-      base09 = "fab387"; # peach
-      base0A = "f9e2af"; # yellow
-      base0B = "a6e3a1"; # green
-      base0C = "94e2d5"; # teal
-      base0D = "89b4fa"; # blue
-      base0E = "cba6f7"; # mauve
-      base0F = "f2cdcd"; # flamingo
-    };
 
     image = ./gruvbox-mountain-village.png;
 
