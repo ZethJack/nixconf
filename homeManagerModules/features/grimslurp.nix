f5e84aa180bf2745893013da4f34ce95d1728cd7
Zeth <zeth@zethjack.eu>
2024-12-26 06:06:30 +0100

    added delay to allow screenshot selection menu to disappear before taking screenshot

--- homeManagerModules/features/grimslurp.nix
+++ homeManagerModules/features/grimslurp.nix
@@ -2,12 +2,24 @@
   grimslurp = pkgs.writeShellScriptBin "grimslurp" ''
 
     case "$(printf "a selected area\\ncurrent window\\nfull screen\\na selected area (copy)\\ncurrent window (copy)\\nfull screen (copy)" | rofi -dmenu -l 6 -i -p "Screenshot which area?")" in
-    	"a selected area") ${pkgs.sway-contrib.grimshot}/bin/grimshot save area $HOME/Pics/Screenshots/pic-selected-"$(date '+%y%m%d-%H%M-%S').png" ;;
-    	"current window") ${pkgs.sway-contrib.grimshot}/bin/grimshot save active $HOME/Pics/Screenshots/pic-window-"$(date '+%y%m%d-%H%M-%S').png" ;;
-    	"full screen") ${pkgs.sway-contrib.grimshot}/bin/grimshot save screen $HOME/Pics/Screenshots/pic-full-"$(date '+%y%m%d-%H%M-%S').png" ;;
-    	"a selected area (copy)") ${pkgs.sway-contrib.grimshot}/bin/grimshot copy area;;
-    	"current window (copy)") ${pkgs.sway-contrib.grimshot}/bin/grimshot copy active;;
-    	"full screen (copy)") ${pkgs.sway-contrib.grimshot}/bin/grimshot copy screen;;
+    	"a selected area")
+        sleep 2 
+        ${pkgs.sway-contrib.grimshot}/bin/grimshot save area $HOME/Pics/Screenshots/pic-selected-"$(date '+%y%m%d-%H%M-%S').png" ;;
+    	"current window")
+        sleep 2 
+        ${pkgs.sway-contrib.grimshot}/bin/grimshot save active $HOME/Pics/Screenshots/pic-window-"$(date '+%y%m%d-%H%M-%S').png" ;;
+    	"full screen") 
+        sleep 2 
+        ${pkgs.sway-contrib.grimshot}/bin/grimshot save screen $HOME/Pics/Screenshots/pic-full-"$(date '+%y%m%d-%H%M-%S').png" ;;
+    	"a selected area (copy)") 
+        sleep 2 
+        ${pkgs.sway-contrib.grimshot}/bin/grimshot copy area;;
+    	"current window (copy)") 
+        sleep 2 
+        ${pkgs.sway-contrib.grimshot}/bin/grimshot copy active;;
+    	"full screen (copy)") 
+        sleep 2 
+        ${pkgs.sway-contrib.grimshot}/bin/grimshot copy screen;;
     esac
   '';
 in {
